e4bb374d291dfcafdec88babcdb0a5c5cec6bcc7
Hameedu Sultan <natlus08@gmail.com>
2018-05-30 20:35:05 +0530

    fix for test failure

--- src/test/java/com/employee/skillset/tracker/employeeskillsettracker/EmployeeSkillsetTrackerApplicationTests.java
+++ src/test/java/com/employee/skillset/tracker/employeeskillsettracker/EmployeeSkillsetTrackerApplicationTests.java
@@ -3,10 +3,16 @@ package com.employee.skillset.tracker.employeeskillsettracker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.employee.skillset.tracker.boot.EmployeeSkillsetTrackerApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest(classes = EmployeeSkillsetTrackerApplication.class)
+@ContextConfiguration
+@ActiveProfiles("local")
 public class EmployeeSkillsetTrackerApplicationTests {
 
 	@Test
