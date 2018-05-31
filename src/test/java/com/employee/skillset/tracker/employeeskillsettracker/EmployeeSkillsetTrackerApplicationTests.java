820595062b6e0753cdfbc408fa37d7f4a20dfd61
natlus08 <natlus08@gmail.com>
2018-05-31 14:08:44 +0530

    port change

--- src/test/java/com/employee/skillset/tracker/employeeskillsettracker/EmployeeSkillsetTrackerApplicationTests.java
+++ src/test/java/com/employee/skillset/tracker/employeeskillsettracker/EmployeeSkillsetTrackerApplicationTests.java
@@ -2,15 +2,11 @@ package com.employee.skillset.tracker.employeeskillsettracker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.employee.skillset.tracker.boot.EmployeeSkillsetTrackerApplication;
-
 @RunWith(SpringRunner.class)
-//@SpringBootTest(classes = EmployeeSkillsetTrackerApplication.class)
 @ContextConfiguration
 @ActiveProfiles("local")
 public class EmployeeSkillsetTrackerApplicationTests {
